3ef11a32224ef1ee7d7ba9d4caf13f02d9257b6c
caojun <caojun@deepblueai.com>
2020-06-02 10:58:02 +0800

    更新utils库，增加简略显示字符串数组函数

--- utils/src/main/java/org/caojun/utils/StringUtils.kt
+++ utils/src/main/java/org/caojun/utils/StringUtils.kt
@@ -1,11 +1,13 @@
 package org.caojun.utils
 
 import android.content.Context
+import java.lang.StringBuilder
 
 object StringUtils {
 
     /**
      * 生成复制后的文件名
+     * @param context
      * @param originalName 原文件名
      * @param names 所有文件名
      */
@@ -16,6 +18,7 @@ object StringUtils {
 
     /**
      * 生成复制后的文件名
+     * @param context
      * @param originalName 原文件名
      * @param names 所有文件名
      */
@@ -38,5 +41,42 @@ object StringUtils {
             return context.getString(R.string.copy_name_copy, copyName, number)
         }
         return copyName
+    }
+
+    /**
+     * 简略显示字符串数组
+     * @param context
+     * @param texts 字符串ArrayList
+     * @param maxNumber 显示最大个数（数组超过maxNumber时最后加上“等/and so on”）
+     */
+    fun mergeStringArray(context: Context, texts: ArrayList<String>, maxNumber: Int = 2): String {
+        val array = texts.toTypedArray()
+        return mergeStringArray(context, array, maxNumber)
+    }
+
+    /**
+     * 简略显示字符串数组
+     * @param context
+     * @param texts 字符串数组
+     * @param maxNumber 显示最大个数（数组超过maxNumber时最后加上“等/and so on”）
+     */
+    fun mergeStringArray(context: Context, texts: Array<String>, maxNumber: Int = 2): String {
+        if (texts.isEmpty()) {
+            return ""
+        }
+        val sb = StringBuilder()
+
+        for (i in texts.indices) {
+            if (sb.isNotEmpty()) {
+                sb.append(context.getString(R.string.punctuation_mark))
+            }
+            sb.append(texts[i])
+            if (i >= maxNumber - 1) {
+                sb.append(context.getString(R.string.and_so_on))
+                break
+            }
+        }
+
+        return sb.toString()
     }
 }
